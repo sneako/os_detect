ac0009a8d4f0d5714ba647a0341db60cc2eb6cf8
Nico Piderman <nico.piderman@gmail.com>
2021-04-28 20:01:47 +0200

    OsDetect v1.0.0
    Now returns a %OsDetect.Result{} struct, which has three fields:
    `os`, `browser`, and `type`. No longer will fallback to regex as I was
    able to test the library against a large dataset of > 3 million distinct
    user agents. Using only binary pattern matching, we are retaining a very
    high level of performance, while achieving a high degree of accuracy.
    In many cases OsDetect has proven to be more accurate than the
    BrowserScope regex libraries that I tested.

--- test/os_detect/patterns_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule OsDetect.PatternsTest do
-  use ExUnit.Case
-
-  test "lists storage contents" do
-    assert [%{regex: regex} | _] = OsDetect.Patterns.list()
-    assert Regex.regex?(regex)
-  end
-end
